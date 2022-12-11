b2077ba53167f720bd1fb07d959d3b5c8bc3c676
Jamila <jamnjeri@gmail.com>
2022-12-11 11:09:59 +0300

    Added list of titles to the left navigation bar

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Linked")
+let baseUrl = 'http://localhost:3000'
+let filmEndPoint = '/films'
+
+let url = `${baseUrl}${filmEndPoint}`
+
+//Function to get the page titles
+let films = fetch(url)
+    .then((films) => films.json())
+    .then((response) => {
+        let films =response
+        // console.log(films);
+
+        for (film of films){
+            // console.log(film.title);
+            createElementFunc(films)
+        }
+    });
+
+function createElementFunc(films){
+    //Create the new element
+    let list = document.createElement('li')
+    list.className = `Title ${film.id}`
+    list.innerText = film.title
+
+    //Get where you want to place them
+    let parentContent = document.querySelector('ul')
+
+    //Add the element
+    parentContent.appendChild(list)
+}
